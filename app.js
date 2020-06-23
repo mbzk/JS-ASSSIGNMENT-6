ba0eb50883e1f97d94f0802e50e9e661c4281495
mbzk <bilalzafar2323@gmail.com>
2020-06-24 00:16:31 +0500

    js code pushed

---
+++ app.js
@@ -0,0 +1,27 @@
+//                 CHAPTER # 21 - 25
+// TASK 1
+// var FirstName = prompt("enter your first name")
+// var LastName = prompt("enter your last name");
+// var FullName = FirstName + LastName;
+// alert(" HELLO! " + " " + FullName )
+
+// TASK 2
+// var FavPhone = prompt("Enter your Favourite Mobile Phone Model");
+// var LengthOfPhone = FavPhone.length;
+// document.write = ("My FAvourite Phone is " + " " + FavPhone); 
+// document.write = ("Length of string is" + " " + LengthOfPhone);
+
+
+
+//  TASK 3
+
+// var Nationality = "pakistani"
+// var index = Nationality.indexOf("n")
+// alert (index)
+
+TASK 4
+
+var greet = "hello world"
+var 
+
+
